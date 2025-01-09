b80fd62f13de312f1971da2380d0de147eec0daa
rohan <rohankhanal114@gmail.com>
2025-01-09 23:18:57 +0545

    chooserole

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { Shield, Heart } from "lucide-react";
+import { Heart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
