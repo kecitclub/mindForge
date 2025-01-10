95af80dc32a32e9d0046ec077e36e4d5b1165999
Gaurab <khanalgaurab98@gmail.com>
2025-01-10 16:32:51 +0545

    Add socket

--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -1,3 +1,4 @@
+import { useSocket } from "@/store/useSocket"
 import { useUserStore } from "@/store/useUserStore"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
@@ -6,12 +7,23 @@ import { useNavigate } from "react-router-dom"
 const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate()
   const { user } = useUserStore()
+  const { setSocket } = useSocket();
 
   useEffect(() => {
     if (!user) {
       navigate("/")
     }
   }, [])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const socket = io('http://localhost:4000');
+      setSocket(socket);
+      return () => {
+        socket.disconnect();
+      };
+    }
+  }, [setSocket]);
 
   return <>{children}</>
 }
